b899aaaecb9c935e69bdf4fdfcec44cc6e8c7ef9
Juan de Dios Quiroz <quirozrodriguezj@gmail.com>
2021-10-30 17:26:21 -0500

    Initial commit

---
+++ src/test/java/com/grupofinanzas/financetrackerbackend/FinanceTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupofinanzas.financetrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
